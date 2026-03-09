0c705016de7447713870d8f237b982cce774d21e
Mark-Eis <mark@markeisler.com>
2026-03-09 16:44:24 +0000

    edit incometax.R; issue #10

--- R/incometax.R
+++ R/incometax.R
@@ -32,13 +32,13 @@ function(x, ...) {
     len <- nchar(tmp)
     maxlen <- max(max(len), 3)
     adj <- attr(x, "allowance") < 0
-    spacer <- if (adj) "      " else ""
+    spacer <- if (adj) "        " else "  "
     cat(
         "Rate:", paste0(paste0(rep(" ", maxlen - 2), collapse = ""), names(x), collapse = ""),
         "\nTax: ", paste0(paste(lapply(maxlen - len + 1, function(x) paste0(rep(" ", x), collapse = ""))), tmp, collapse = ""),
-        "\nTotal tax:", spacer, prettyL(sum(x)), 
+        paste("\nTotal tax:", prettyL(sum(x)), sep = spacer), 
         if (adj) paste("\nPay adjustment:" , prettyL(abs(attr(x, "allowance"))), sep = "   "),
-        "\nAllowance: ", if (adj) "      £0" else prettyL(attr(x, "allowance")),
+        paste("\nAllowance:", prettyL(if (adj) 0 else attr(x, "allowance")), sep = spacer),
         if (adj) "\nAdjusted Taxable:" else "\nTaxable:   ", prettyL(attr(x, "taxable")), "\n\n")
 }
 

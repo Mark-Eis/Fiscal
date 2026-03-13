f4ee46b703db857184e60851d6214abf84a488c4
Mark-Eis <mark@markeisler.com>
2026-03-13 21:16:17 +0000

    edit allowance.R; issue #13

--- R/allowance.R
+++ R/allowance.R
@@ -8,14 +8,14 @@ function(taxable, deficit = NULL, opts = tax_opts()) {
 			deficit <- deficit %||% sum(as.double(incometax(taxable)))
 			allow <- 
 			if (deficit > band[1] * rate[1] + band[2] * rate[2])
-				taxable - (band[1] + band[2] - 1 + (deficit - band[1] * rate[1] - (band[2] - 1) * rate[2]) / rate[3])
+				taxable - (band[1] + band[2] + (deficit - band[1] * rate[1] - band[2] * rate[2]) / rate[3])
 			else
 				if (deficit > band[1] * rate[1])
 					taxable - (band[1] + (deficit - band[1] * rate[1]) / rate[2])
 				else
 					taxable - deficit / rate[1]
 			offset <- if (allow < 0) 0 else 9
-			allow - max(min(((sum(band) - taxable) / 2 + 9), offset), 0)		# adjustment as for incometax()		
+			allow - max(min(((sum(band) + 1 - taxable) / 2 + 9), offset), 0)		# adjustment as for incometax()		
 		}
 	)
 }
